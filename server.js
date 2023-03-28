5e581ef4c839615179a3ec3f32a00aaeb9831efb
Georgefdz <georgfdz@gmail.com>
2023-03-27 20:16:15 -0600

    added some comments

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-const passport = require('passport')
+const mongoose = require('mongoose')// This is to connect to the database
+const passport = require('passport')//Handles our authentication, it has strategies for different types of authentication, we are using local strategy
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
-const logger = require('morgan')
+const MongoStore = require('connect-mongo')(session) // Help us have logged in users (handling cookies and sessions)
+const flash = require('express-flash')// This is to display flash messages (verification like "You are logged in")
+const logger = require('morgan') // This is to log requests to the console
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'})// This is to load the environment variables from the .env file
 
 // Passport config
 require('./config/passport')(passport)
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(logger('dev'))
+app.use(logger('dev'))// This is to log activate morgan and log requests to the console
 
 
 // Sessions (This is to store the user's session)
